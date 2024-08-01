0ebe68e3819ed2cd3e0ebf126ba74ff28fef0caf
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2024-08-01 14:11:41 +0200

    Add D based MIP

--- src/linear_program.jl
+++ src/linear_program.jl
@@ -70,3 +70,49 @@ function MIP_reduced(g, D::Matrix, c; relax = true) # D is a distance matrix (2n
     end
     return model
 end
+
+# Non-graph based version of the MIP. 
+function MIP_complete(D::Matrix; relax = true, semi_relax = false, init_tau = zeros(D)) # D is a distance matrix (n x n)
+    n = size(D,1)
+    TAXA = 1:n
+    taxon1 = first(TAXA)
+    TAXA2n = TAXA[2:end]
+    LENGTHS = 2:n-1
+    model = Model(OPTIMIZER, add_bridges = false)
+    set_string_names_on_creation(model, false)
+    if semi_relax
+        @variable(model, 0<=_x[i=TAXA, j=TAXA, l=LENGTHS; i<j]<=1, start = init_tau[i,j] == l ? 1 : 0)
+    else
+        @variable(model, _x[i=TAXA, j=TAXA, l=LENGTHS; i<j], Bin, start = init_tau[i,j] == l ? 1 : 0)
+    end
+    @variable(model, _y[j = TAXA2n, p = TAXA2n, q = TAXA2n; j ≠ p && j ≠ q && p < q], Bin)
+    
+    @expression(model, y[j = TAXA2n, p = TAXA2n, q = TAXA2n; j ≠ p && j ≠ q && p ≠ q], _y[j, minmax(p,q)...])
+    @expression(model, x[i=TAXA, j=TAXA, l=LENGTHS; i≠j], _x[minmax(i,j)...,l])
+    @expression(model, τ[i=TAXA, j=TAXA; i≠j], sum(l*_x[minmax(i,j)...,l] for l in LENGTHS))
+
+    @constraints model begin
+        c_one_length[i=TAXA, j=TAXA; i<j], 
+            sum(x[i,j,l] for l in LENGTHS) == 1
+        c_kraft[i=TAXA], 
+            sum(sum(exp2(-l)*x[i,j,l] for l in LENGTHS) for j in TAXA if j ≠ i)  == 0.5
+        c_manifold, 
+            sum(l*exp2(-l)*x[i,j,l] for i in TAXA, j in TAXA, l in LENGTHS if i≠j) == (2n-3)
+        
+        c_str_triangular[i=TAXA2n, j=TAXA2n; i ≠ j],
+            τ[taxon1, i] + τ[taxon1, j] - τ[i,j] >= 2
+        c_str_triangular2[i=TAXA2n, j=TAXA2n; i ≠ j],
+            τ[taxon1, i] + τ[i,j] - τ[taxon1,j] >= 2
+        
+        c_bun_sum[j = TAXA2n, p = TAXA2n, q = TAXA2n; j ≠ p && j ≠ q && p ≠ q],
+            y[j,p,q] + y[p,j,q] + y[q,j,p] == 1            
+        c_buneman[j = TAXA2n, p = TAXA2n, q = TAXA2n; j ≠ p && j ≠ q && p ≠ q],
+            τ[taxon1,j] + τ[p,q] >= 2(1 - y[q,j,p]) + τ[taxon1,p] + τ[j,q] - (2n - 2)y[j,p,q]
+    end
+
+    @objective(model, Min, sum(D[i,j]*sum(exp2(-l)*x[i,j,l] for l in LENGTHS) for i in TAXA, j in TAXA if i≠j))
+    if relax
+        relax_integrality(model)
+    end
+    return model
+end

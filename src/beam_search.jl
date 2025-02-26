95fb4a820814587ed85b5b2530f333334af62839
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2025-02-26 16:14:38 +0100

    optimize heaps

--- src/beam_search.jl
+++ src/beam_search.jl
@@ -1,4 +1,20 @@
 export beam_search, beam_search_lp
+using DataStructures
+
+struct State 
+    graph::SimpleGraph
+    D::Matrix{Float64}
+    l::Float64
+    star_tips::Dict{Int,Int}
+end
+
+struct Cherry
+    cherry::Tuple{Int,Int}
+    Q::Float64
+    parent::State
+end
+
+Base.isless(c::Cherry, d::Cherry) = c.Q < d.Q
 
 function reduce_matrix(D, i, j, v, star_tips)
     n = only(unique(size(D)))
@@ -17,10 +33,11 @@ function beam_search_lp(D_start, B; triangular = true, buneman = false, scale = 
     for model in models
         set_silent(model.second)
     end
-    states = [(graph = copy(_g), D = D_start, l = 0.0, star_tips = Dict(i => n for (i,n) in enumerate(neighbors(_g, c))))]
+    states = [State(copy(_g), D_start, 0.0, Dict(i => n for (i,n) in enumerate(neighbors(_g, c))))]
     K = n
     while K > 3
-        heap = @NamedTuple{cherry::Tuple{Int,Int}, Q::Float64, parent::eltype(states)}[] # the heap could be optimized with an actual heap DS.
+        # heap = @NamedTuple{cherry::Tuple{Int,Int}, Q::Float64, parent::eltype(states)}[] # the heap could be optimized with an actual heap DS.
+        heap = BinaryMinHeap{Cherry}()
         for state in states
             D = state.D
             model = models[K]
@@ -31,16 +48,13 @@ function beam_search_lp(D_start, B; triangular = true, buneman = false, scale = 
             τ = value.(model[:τ])
             for (i,j) in combinations(candidates, 2)
                 lb_delta = D[i,j]/2 - D[i,j]/exp2(τ[i,j]-1)
-                push!(heap, (cherry = (i,j), Q = LB + state.l + lb_delta, parent = state))
+                push!(heap, Cherry((i,j), LB + state.l + lb_delta, state))
             end
         end
-        sort!(heap, rev = false, by = p -> p.Q)
         empty!(states)
         split_sets = Set{Set{Tuple{Set{Int}, Set{Int}}}}()
-        idx = 0
-        while length(states) < B && idx <= length(heap)
-            idx += 1
-            child = heap[idx]
+        while length(states) < B && !isempty(heap)
+            child = pop!(heap)
             gchild = copy(child.parent.graph)
             star_tips = child.parent.star_tips
             D = child.parent.D
@@ -48,10 +62,15 @@ function beam_search_lp(D_start, B; triangular = true, buneman = false, scale = 
             i, j = star_tips[i_model], star_tips[j_model]
             v = join_neighbors!(gchild, c, i, j)
             splits = compute_splits(gchild)
-            if splits ∉ split_sets
-                push!(split_sets, splits)
+            if B > 1
+                if splits ∉ split_sets
+                    push!(split_sets, splits)
+                    D_check, star_tips = reduce_matrix(copy(D), i_model, j_model, v, star_tips) # averages
+                    push!(states, State(gchild,D_check,child.parent.l + D[i_model,j_model]/2, star_tips))
+                end
+            else
                 D_check, star_tips = reduce_matrix(copy(D), i_model, j_model, v, star_tips) # averages
-                push!(states, (graph = gchild, D = D_check, l = child.parent.l + D[i_model,j_model]/2, star_tips = star_tips))
+                push!(states, State(gchild,D_check,child.parent.l + D[i_model,j_model]/2, star_tips))
             end
         end
         K -= 1
@@ -66,10 +85,10 @@ end
 function beam_search(D_start, B; spr = true)
     _g, c = star_graph(D_start)
     n = only(unique(size(D_start)))
-    states = [(graph = copy(_g), D = D_start, l = 0.0, star_tips = Dict(i => n for (i,n) in enumerate(neighbors(_g, c))))]
+    states = [State(copy(_g), D_start, 0.0, Dict(i => n for (i,n) in enumerate(neighbors(_g, c))))]
     K = n
     while K > 3
-        heap = @NamedTuple{cherry::Tuple{Int,Int}, Q::Float64, parent::eltype(states)}[] # the heap could be optimized with an actual heap DS.
+        heap = BinaryMinHeap{Cherry}()
         for state in states
             D = state.D
             candidates = 1:K
@@ -77,27 +96,29 @@ function beam_search(D_start, B; spr = true)
             sum_D = sum(col_sums)
             for (i,j) in combinations(candidates, 2)
                 star_tree_length = (sum_D - col_sums[i] - col_sums[j])/(2(K-2))
-                push!(heap, (cherry = (i,j), Q = state.l + D[i,j]/2 + star_tree_length, parent = state))
+                push!(heap, Cherry((i,j), state.l + D[i,j]/2 + star_tree_length, state))
             end
         end
-        sort!(heap, rev = false, by = p -> p.Q)
         empty!(states)
         split_sets = Set{Set{Tuple{Set{Int}, Set{Int}}}}()
-        idx = 0
-        while length(states) < B && idx <= length(heap)
-            idx += 1
-            child = heap[idx]
+        while length(states) < B && !isempty(heap)
+            child = pop!(heap)
             gchild = copy(child.parent.graph)
             star_tips = child.parent.star_tips
             D = child.parent.D
             i_model, j_model = child.cherry
             i, j = star_tips[i_model], star_tips[j_model]
             v = join_neighbors!(gchild, c, i, j)
-            splits = compute_splits(gchild)
-            if splits ∉ split_sets
-                push!(split_sets, splits)
+            if B > 1
+                splits = compute_splits(gchild)
+                if splits ∉ split_sets
+                    push!(split_sets, splits)
+                    D_check, star_tips = reduce_matrix(copy(D), i_model, j_model, v, star_tips) # averages
+                    push!(states, State(gchild, D_check, child.parent.l + D[i_model,j_model]/2, star_tips))
+                end
+            else
                 D_check, star_tips = reduce_matrix(copy(D), i_model, j_model, v, star_tips) # averages
-                push!(states, (graph = gchild, D = D_check, l = child.parent.l + D[i_model,j_model]/2, star_tips = star_tips))
+                push!(states, State(gchild, D_check, child.parent.l + D[i_model,j_model]/2, star_tips))
             end
         end
         K -= 1
